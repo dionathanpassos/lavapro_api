22577df57cdc16e87b9f47e37ed5a917bf32e751
Dionathan Passos <devdionathanpassos@gmail.com>
2026-05-20 22:47:10 -0300

    Projeto Spring inicializado

---
+++ src/main/java/com/dionathan/lavapro/LavaproApplication.java
@@ -0,0 +1,13 @@
+package com.dionathan.lavapro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavaproApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LavaproApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dionathan/lavapro/LavaproApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dionathan.lavapro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavaproApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
